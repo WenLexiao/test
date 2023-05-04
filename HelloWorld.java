13e7010cca91c3ea1eed6906ca8878e00b432c6e
unknown <lexiao0524@gmail.com>
2023-05-04 18:19:00 +0800

    main 分支

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void method02(){
+        System.out.println("main 分支!");
+        
+    }
 }
