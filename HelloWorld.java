a2639b252c33639be842ec2d7086aec84816c008
unknown <lexiao0524@gmail.com>
2023-05-04 18:18:16 +0800

    dev方法

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void method01(){
+        System.out.println("dev方法!");
+    }
 }
